2064d1516e13a82cc531b646a4ec07c039dfef90
irahulmalik <rahulmalik1995@gmail.com>
2022-05-27 11:27:31 +0530

    1 file modified

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -11,7 +11,7 @@ export class DataService {
   mytodos: Array<todo>;
 
   constructor() {
-  // private apiService: ApiService
+    // private apiService: ApiService
     this.mytodos = [
       { id: 1, status: status.TODO, todo: 'Hey this is 1irst' },
       { id: 2, status: status.INPROGRESS, todo: 'Hey this is 2nd' },
@@ -19,16 +19,24 @@ export class DataService {
     ];
   }
 
-  getAllTodos(): any { // Observable<Array<todo>>
+  getAllTodos(): any {
+    // Observable<Array<todo>>
     // return this.apiService.getAllTodos();
-    console.log("called")
+    console.log('called');
     return this.mytodos;
   }
 
-  saveTodo(todo: todo) {
+  saveTodo(todos: string) {
     // return this.apiService.saveTodo(todo);
-
-    console.log("saving")
+    let newId = this.mytodos.length + 1;
+    let newTodo: todo = {
+      id: newId,
+      todo: todos,
+      status: status.TODO,
+    };
+    this.mytodos.push(newTodo);
+    console.log('saving');
+    console.log(this.mytodos);
   }
   updateTodo(todo: todo) {
     // return this.apiService.updateTodo(todo);
